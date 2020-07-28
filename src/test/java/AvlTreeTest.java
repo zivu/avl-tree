19f9c806acbbbda7b52313f49874af49c45b9ad8
zivu <rj.pavlo.zvarych@gmail.com>
2020-07-28 10:15:19 +0200

    tests for avl

--- src/test/java/AvlTreeTest.java
+++ src/test/java/AvlTreeTest.java
@@ -1,6 +1,9 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
 class AvlTreeTest {
 
     @Test
@@ -95,7 +98,7 @@ class AvlTreeTest {
         //when
         int maxNodes = avlTree.maxNodes(root);
         //then
-        Assertions.assertEquals(0, maxNodes);
+        assertEquals(0, maxNodes);
     }
 
     @Test
@@ -109,7 +112,103 @@ class AvlTreeTest {
         //when
         int maxNodes = avlTree.maxNodes(root);
         //then
-        Assertions.assertEquals(2, maxNodes);
+        assertEquals(2, maxNodes);
+    }
+
+    @Test
+    public void shouldThrowNPEWhenProvidedWithNullArgument() {
+        //given
+        Node nullNode = null;
+        AvlTree avlTree = new AvlTree();
+        //when
+        Assertions.assertThrows(NullPointerException.class, () -> avlTree.rebalance(nullNode));
+        //then NPE is thrown
+    }
+
+    @Test
+    public void shouldDoRightRotation() {
+        //given
+        AvlTree avlTree = new AvlTree();
+        Node grandParentNode = new Node(30);
+        Node parent = new Node(20);
+        parent.setParent(grandParentNode);
+        Node child = new Node(10);
+        child.setParent(parent);
+        grandParentNode.setLeft(parent);
+        parent.setLeft(child);
+        //when
+        avlTree.rebalance(grandParentNode);
+        //then
+        assertNull(parent.getParent());
+        assertNull(grandParentNode.getLeft());
+        assertEquals(grandParentNode.getParent(), parent);
+        assertEquals(parent.getRight(), grandParentNode);
+    }
+
+    @Test
+    public void shouldDoLeftRotation() {
+        //given
+        AvlTree avlTree = new AvlTree();
+        Node grandParent = new Node(10);
+        Node parent = new Node(20);
+        Node child = new Node(30);
+        grandParent.setRight(parent);
+        parent.setRight(child);
+        child.setParent(parent);
+        parent.setParent(grandParent);
+        //when
+        avlTree.rebalance(grandParent);
+        //then
+        assertEquals(parent, grandParent.getParent());
+        assertNull(grandParent.getRight());
+        assertNull(parent.getParent());
+        assertEquals(grandParent, parent.getLeft());
+    }
+
+    @Test
+    public void shouldDoLeftRightRotation() {
+        //given
+        AvlTree avlTree = new AvlTree();
+        Node grandParent = new Node(30);
+        Node parent = new Node(20);
+        Node child = new Node(25);
+        grandParent.setLeft(parent);
+        parent.setRight(child);
+        child.setParent(parent);
+        parent.setParent(grandParent);
+        //when
+        avlTree.rebalance(grandParent);
+        //then
+        assertNull(parent.getRight());
+        assertEquals(child, parent.getParent());
+        assertEquals(parent, child.getLeft());
+        assertEquals(grandParent, child.getRight());
+        assertNull(child.getParent());
+        assertEquals(child, grandParent.getParent());
+        assertNull(grandParent.getLeft());
+    }
+
+    @Test
+    public void shouldDoRightLeftRotation() {
+        //given
+        AvlTree avlTree = new AvlTree();
+        Node grandParent = new Node(20);
+        Node parent = new Node(30);
+        Node child = new Node(25);
+        child.setParent(parent);
+        parent.setParent(grandParent);
+        grandParent.setRight(parent);
+        parent.setLeft(child);
+        //when
+        avlTree.rebalance(grandParent);
+        //then
+        assertNull(grandParent.getRight());
+        assertEquals(child, grandParent.getParent());
+        assertNull(child.getParent());
+        assertEquals(grandParent, child.getLeft());
+        assertEquals(parent, child.getRight());
+        assertEquals(child, parent.getParent());
+        assertNull(parent.getLeft());
     }
 
 }
